6d5ea221a7b4acfd9595460908499096e2190ca5
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-05-15 13:24:03 +0200

    Initial commit

---
+++ backend/todo/src/test/java/com/simonekouters/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonekouters.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
